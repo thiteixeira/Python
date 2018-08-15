0c4d56126afd7b0ab611ae72425d9d6910384f84
Thiago Teixeira <thiteixeira@gmail.com>
2018-08-15 18:54:31 -0400

    fixed errors

--- networking/TCPClient.py
+++ networking/TCPClient.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
+
 from socket import *
 
-serverName = "10.0.0.250"
+serverName = "127.0.0.1"
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
 sentence = input("Input lowercase sentence:")
-clientSocket.send(sentence)
+clientSocket.send(sentence.encode())
 
 modifiedSentence = clientSocket.recv(1024)
 
--- networking/TCPServer.py
+++ networking/TCPServer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from socket import *
 
 serverPort = 12000
@@ -5,7 +7,7 @@ serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
 serverSocket.listen(1)
 
-print ("The server is ready to receive")
+print("The server is ready to receive")
 
 while 1:
     connectionSocket, addr = serverSocket.accept()
