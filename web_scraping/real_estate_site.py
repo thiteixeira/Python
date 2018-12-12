20adaec5589445d68b5fa79c773b8c07e76b9c1a
Thiago <thiteixeira@gmail.com>
2018-12-12 11:57:46 -0500

    Update real_estate_site.py

--- web_scraping/real_estate_site.py
+++ web_scraping/real_estate_site.py
@@ -13,6 +13,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'http://www.procuraseimovel.com.br/portal/imoveis/rs/rio-grande/venda?pagina='
+limit = 300000  # floor price. Do not consider anything lower than this price
 
 
 def main():
@@ -30,11 +31,18 @@ def main():
     for p, t in zip(prices, titles):
       price = re.sub("[^0-9|,]", "", p.text).replace(",", ".")
       if len(price) > 0:
-        if float(price) > 400000:
+        if float(price) > limit:
           print(float(price), t.text)
+          
+          # TODO: Put in a Pandas DataFrame, extract the bedroom, baths,
+          # and garage information and plot the statistics
+          
+          # TODO: Get address to improve price prediction
+          
+          # TODO: Use Machine Learning (Linear Regression?) to predict the
+          # prices
       
-    page += 1  
-
+    page += 1
   
 if __name__ == "__main__":
   main()
