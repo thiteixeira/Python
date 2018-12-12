6944b4dfff61dacb87bc3c0102646d541213a5fb
Thiago <thiteixeira@gmail.com>
2018-12-12 11:45:10 -0500

    Create real_estate_site.py

---
+++ web_scraping/real_estate_site.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+'''
+This code aims to get the price and description of estates in the city
+of Rio Grande, RS, Brazil. 
+
+Author: Thiago Teixeira
+'''
+
+import pandas as pd
+import re
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.procuraseimovel.com.br/portal/imoveis/rs/rio-grande/venda?pagina='
+
+
+def main():
+  max_pages = 10
+  page = 1
+  while page < max_pages:
+    res = requests.get(url + str(page) + '_2_0')
+    if res.status_code != 200:
+      return False
+    
+    so = BeautifulSoup(res.text, "lxml")
+    titles = so.findAll('p', {'class': 'descricao'})
+    prices = so.findAll('div', {'class': 'direita'})
+    
+    for p, t in zip(prices, titles):
+      price = re.sub("[^0-9|,]", "", p.text).replace(",", ".")
+      if len(price) > 0:
+        print(t.text)
+        print(float(price))
+      
+    page += 1  
+
+  
+if __name__ == "__main__":
+  main()
